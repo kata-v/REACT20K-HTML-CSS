517f6c10c09ca1b2bd0c4c9fc93be049699c82c0
KataV <KataV@users.noreply.github.com>
2020-03-19 12:28:20 +0200

    Fixed the bug with the Back to Top Button
    Added a missing </i> tag.

--- 16_Jobs_In_Finland/scripts.js
+++ 16_Jobs_In_Finland/scripts.js
@@ -1,20 +1,20 @@
-//Script for "back to top" button, code from w3school
+// Script for "back to top" button, code from w3school
 
-// var mybutton = document.getElementById("toTopBtn");
+var mybutton = document.getElementById("toTopBtn");
 
-// window.onscroll = function() {
-//   scrollFunction();
-// };
+window.onscroll = function() {
+  scrollFunction();
+};
 
-// function scrollFunction() {
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//     mybutton.style.display = "block";
-//   } else {
-//     mybutton.style.display = "none";
-//   }
-// }
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
 
-// function topFunction() {
-//   document.body.scrollTop = 0; // For Safari
-//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-// }
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
